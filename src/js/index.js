9d5830b5b500d49027dd2e4c7ef0599ee94edb53
R-A-S <r.a.s@live.ru>
2019-11-24 12:48:24 +0200

    docs: 2 Test eslint with warning without errors + commitizen

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import '../styles/styles.scss';
+
+console.log('→ ', 'Hello world!👶');
+
+console.log('→ ', 'Hello world!👶');
+const myArrowFunc = (a, b) => {
+  if (a === undefined) return b;
+  return a;
+};
+myArrowFunc(11);
